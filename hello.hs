5f53072464d0522ed8a7f74b339829253188b662
Dan Brown <dbbnrl@gmail.com>
2015-08-05 21:00:24 -0400

    Add type signatures.

--- hello.hs
+++ hello.hs
@@ -1,21 +1,25 @@
-{-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell #-}
+{-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell, PartialTypeSignatures #-}
 import Data.Bool
 import Reflex.Dom
 
+main :: IO ()
 main = mainWidget stuff
 
 -- flipflop a b e = do
 --   tog <- toggle False e
 --   mapDyn (bool a b) tog
 
+flipflop :: (_) => a -> a -> Event t b -> m (Dynamic t a)
 flipflop a b e = toggle False e >>= mapDyn (bool a b)
 
+mybutton :: MonadWidget t m => m ()
 mybutton = do
     rec (bel, _) <- el' "button" $ dynText btext
         let clicked = domEvent Click bel
         btext <- flipflop "unclicked" "Clicked!" clicked
     return ()
 
+stuff :: MonadWidget t m => m ()
 stuff = do
     text "Hello, world!"
     text "blah"
