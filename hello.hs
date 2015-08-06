1e10dbc906100b0c028ab8e2bae09690962206a8
Dan Brown <dbbnrl@gmail.com>
2015-08-06 16:40:55 -0400

    Added counting button.

--- hello.hs
+++ hello.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell, PartialTypeSignatures #-}
 import Data.Bool
 import Reflex.Dom
+import Control.Monad (void)
 
 main :: IO ()
 main = mainWidget stuff
@@ -19,8 +20,21 @@ mybutton = do
         btext <- flipflop "unclicked" "Clicked!" clicked
     return ()
 
+counterButton :: MonadWidget t m => m (Dynamic t Int)
+counterButton = do
+    rec (bel, _) <- el' "button" $ display cnt
+        let clicks = domEvent Click bel
+        cnt <- count clicks
+    return cnt
+
+-- bunchaButtons :: MonadWidget t m => m ()
+-- bunchaButtons = do
+--     addEv <- counterButton
+
 stuff :: MonadWidget t m => m ()
 stuff = do
     text "Hello, world!"
     text "blah"
+    -- bunchaButtons
     mybutton
+    void counterButton
