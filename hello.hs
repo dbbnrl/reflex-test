51cadaccec9a2e257a9fe234d64641db07d843e8
Dan Brown <dbbnrl@gmail.com>
2015-08-10 08:41:58 -0400

    In progress.

--- hello.hs
+++ hello.hs
@@ -3,10 +3,11 @@ import Data.Bool
 import Reflex.Dom
 import Control.Monad (void)
 import qualified Data.Map.Strict as Map
-import Data.Map.Strict (Map)
+import Data.Map.Strict (Map, mapWithKey)
 import Data.Foldable (foldl')
 import Data.Monoid ((<>))
 import Data.ByteString (ByteString)
+import Data.List (intercalate)
 
 chooseDyn :: (_) => Dynamic t a -> Dynamic t a -> Dynamic t Bool -> m (Dynamic t a)
 chooseDyn x y b = joinDyn <$> mapDyn (bool x y) b
@@ -34,32 +35,40 @@ mybutton = do
 
 counterButton :: MonadWidget t m => m (Event t Int)
 counterButton = do
-  rec (bel, _) <- el' "button" $ display cnt
-      let clicks = domEvent Click bel
-      cnt <- count clicks
+  (bel, _) <- el' "button" $ text "New"
+  let clicks = domEvent Click bel
+  cnt <- count clicks
   return $ updated cnt
 
 mkNode :: (MonadWidget t m, _) => k -> Dynamic t [(Int, Bool)] -> m (Dynamic t (Int, Bool))
 mkNode k ports = do
   levelIn <- forDyn ports $ (foldl' max 0) . fmap fst 
   levelOut <- mapDyn succ levelIn
-  let constLabel = "I am #" <> show k <> " @"
-  label <- ((constLabel <>) . show) `mapDyn` levelOut
-  root <- mapDyn (== 0) levelIn
+  levelName <- mapDyn show levelIn
+  (portNames :: Dynamic t String) <- forDyn ports $ (intercalate ",") . (fmap (show . fst))
+  let constLabel = constDyn $ "I am #" <> show k <> " @"
+  label <- mconcatDyn [constLabel, levelName, constDyn "\n", portNames]
+  atRoot <- mapDyn (== 0) levelIn
+  rootAttr <- forDyn atRoot (bool ("disabled" =: "true") Map.empty)
   stateIn <- mapDyn (and . fmap snd) ports
-  rec stateOut <- chooseDyn stateIn manual root
-      stateAttr <- mapDyn (bool Map.empty ("class" =: "active")) stateOut
-      (bel, _) <- elDynAttr' "button" stateAttr $ do
+  rec stateOut <- chooseDyn stateIn clickToggle atRoot
+      stateAttr <- forDyn stateOut (bool Map.empty ("class" =: "active"))
+      nodeAttrs <- mconcatDyn [stateAttr, rootAttr]
+      (bel, _) <- elDynAttr' "button" nodeAttrs $ do
         dynText label
-      manual <- toggle False $ domEvent Click bel
+      clickToggle <- toggle False $ domEvent Click bel
   combineDyn (,) levelOut stateOut
 
 bunchaButtons :: MonadWidget t m => m ()
 bunchaButtons = do
-  cnt <- divClass "control" counterButton
-  let insertNew = fmap newKey cnt
-  let bmanip = leftmost [ fmap newKey cnt ]
-  nodeIn <- foldDyn ($) Map.empty bmanip
+  rec (cnt, from, to) <- divClass "control" $ do
+        c <- counterButton
+        (Dropdown f _) <- dropdown 0 keyNames def
+        (Dropdown t _)  <- dropdown 0 keyNames def
+        return (c, f, t)
+      let bmanip = leftmost [ fmap newKey cnt ]
+      nodeIn <- foldDyn ($) Map.empty bmanip
+      keyNames <- forDyn nodeIn $ (fmap show) . (mapWithKey const)
   nodeOut <- listWithKey nodeIn mkNode
   return ()
   where
