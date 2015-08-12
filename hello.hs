86e23824cf0c216a0ce280cf3ec9cd420fe742b0
Dan Brown <dbbnrl@gmail.com>
2015-08-12 19:33:17 -0400

    Reorganize for clarity.

--- hello.hs
+++ hello.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell, PartialTypeSignatures, OverloadedStrings #-}
+{-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell, PartialTypeSignatures, OverloadedStrings, BangPatterns #-}
 import Data.Bool
 import Reflex.Dom
 import Control.Monad (void)
@@ -12,26 +12,7 @@ import Data.List (intercalate)
 chooseDyn :: (_) => Dynamic t a -> Dynamic t a -> Dynamic t Bool -> m (Dynamic t a)
 chooseDyn x y b = joinDyn <$> mapDyn (bool x y) b
 
-myCss :: ByteString = "\
-  \.active {color: #00ff00;}\
-  \"
-
-main :: IO ()
-main = mainWidgetWithCss myCss stuff
-
--- flipflop a b e = do
---   tog <- toggle False e
---   mapDyn (bool a b) tog
-
-flipflop :: (_) => a -> a -> Event t b -> m (Dynamic t a)
-flipflop a b e = toggle False e >>= mapDyn (bool a b)
-
-mybutton :: MonadWidget t m => m ()
-mybutton = do
-  rec (bel, _) <- el' "button" $ dynText btext
-      let clicked = domEvent Click bel
-      btext <- flipflop "unclicked" "Clicked!" clicked
-  return ()
+data Node = Node {nLevel :: !Int, nVal :: !Bool}
 
 counterButton :: MonadWidget t m => m (Event t Int)
 counterButton = do
@@ -40,25 +21,38 @@ counterButton = do
   cnt <- count clicks
   return $ updated cnt
 
-linkButton :: MonadWidget t m => Dynamic t k -> Dynamic t (Map k (Dynamic t (Int, Bool))) -> m (Event t (k, (Map k (Dynamic t (Int, Bool)))))
+linkButton :: MonadWidget t m => Dynamic t k -> Dynamic t (Map k (Dynamic t Node)) -> m (Event t (k, (Map k (Dynamic t Node))))
 linkButton to newPort = do
   (bel, _) <- el' "button" $ text "Link"
   let clicks = domEvent Click bel
   return $ attachDyn to $ tagDyn newPort clicks
 
-mkNode :: (MonadWidget t m, _) => k -> Dynamic t (Map k (Dynamic t (Int, Bool))) -> m (Dynamic t (Int, Bool))
-mkNode k p = do
-  let ports = joinDynThroughMap p
-  levelIn <- forDyn ports $ (foldl' max 0) . fmap fst 
+controlPanel :: (MonadWidget t m, _) => Dynamic t (Map k (Dynamic t Node)) -> m (Event t Int, Event t (k, (Map k (Dynamic t Node))))
+controlPanel state = do
+  keyNames <- forDyn state $ (fmap show) . (mapWithKey const)
+  divClass "control" $ do
+    c <- counterButton
+    (Dropdown from _) <- dropdown 0 keyNames def
+    (Dropdown to _)  <- dropdown 0 keyNames def
+    newPort <- combineDyn getPort from state
+    p <- linkButton to newPort
+    return (c, p)
+  where
+    getPort from m = maybe Map.empty (from =:) (Map.lookup from m)
+
+mkNode :: (MonadWidget t m, _) => k -> Dynamic t (Map k (Dynamic t Node)) -> m (Dynamic t Node)
+mkNode k s = do
+  let sources = joinDynThroughMap s
+  levelIn <- forDyn sources $ (foldl' max 0) . fmap nLevel 
   levelOut <- mapDyn succ levelIn
   levelName <- mapDyn show levelIn
-  (portNames :: Dynamic t String) <- forDyn ports $ (intercalate ",") . (fmap show) . Map.keys
+  (sourceLabels :: Dynamic t String) <- forDyn sources $ (intercalate ",") . (fmap show) . Map.keys
   let constLabel = constDyn $ "I am #" <> show k <> " @Level "
   label1 <- mconcatDyn [constLabel, levelName]
-  label2 <- mconcatDyn [constDyn "Sources: ", portNames]
+  label2 <- mconcatDyn [constDyn "Sources: ", sourceLabels]
   atRoot <- mapDyn (== 0) levelIn
   rootAttr <- forDyn atRoot (bool ("disabled" =: "true") Map.empty)
-  stateIn <- mapDyn (and . fmap snd) ports
+  stateIn <- mapDyn (and . fmap nVal) sources
   rec stateOut <- chooseDyn stateIn clickToggle atRoot
       stateAttr <- forDyn stateOut (bool Map.empty ("class" =: "active"))
       nodeAttrs <- mconcatDyn [stateAttr, rootAttr]
@@ -66,30 +60,27 @@ mkNode k p = do
         el "p" $ dynText label1
         el "p" $ dynText label2
       clickToggle <- toggle False $ domEvent Click bel
-  combineDyn (,) levelOut stateOut
+  combineDyn Node levelOut stateOut
 
-bunchaButtons :: MonadWidget t m => m ()
-bunchaButtons = do
-  rec (cnt, newp) <- divClass "control" $ do
-        c <- counterButton
-        (Dropdown from _) <- dropdown 0 keyNames def
-        (Dropdown to _)  <- dropdown 0 keyNames def
-        newPort <- combineDyn getPort from nodeOut
-        p <- linkButton to newPort
-        return (c, p)
-      let bmanip = leftmost [ fmap newKey cnt, fmap addPort newp ]
-      (nodeIn :: Dynamic t (Map Int (Map Int (Dynamic t (Int, Bool))))) <- foldDyn ($) Map.empty bmanip
-      (nodeOut :: Dynamic t (Map Int (Dynamic t (Int, Bool)))) <- listWithKey nodeIn mkNode
-      keyNames <- forDyn nodeOut $ (fmap show) . (mapWithKey const)
+network :: MonadWidget t m => m ()
+network = do
+  rec (newClicked, linkClicked) <- controlPanel stateOut
+      let stateManip = leftmost [ fmap newNode newClicked, fmap addPort linkClicked ]
+      (stateIn :: Dynamic t (Map Int (Map Int (Dynamic t Node)))) <- foldDyn ($) Map.empty stateManip
+      (stateOut :: Dynamic t (Map Int (Dynamic t Node))) <- listWithKey stateIn mkNode
   return ()
   where
-    newKey k = Map.insert k Map.empty
-    getPort from m = maybe Map.empty (from =:) (Map.lookup from m)
+    newNode k = Map.insert k Map.empty
     addPort (to, p) = Map.adjust (p <>) to
 
-stuff :: MonadWidget t m => m ()
-stuff = do
-  text "Hello, world!"
-  text "blah"
-  bunchaButtons
-  -- mybutton
+body :: MonadWidget t m => m ()
+body = do
+  el "h1" $ text "Dynamic Graph Test"
+  network
+
+myCss :: ByteString = "\
+  \.active {color: #00ff00;}\
+  \"
+
+main :: IO ()
+main = mainWidgetWithCss myCss body
