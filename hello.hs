7f640a7c590c120200f7a4a840a53a3fbfbc1528
Dan Brown <dbbnrl@gmail.com>
2015-08-12 23:12:35 -0400

    Switch to monadic manipulator events.

--- hello.hs
+++ hello.hs
@@ -35,39 +35,29 @@ delButton which = do
   let clicks = domEvent Click bel
   return $ tagDyn which clicks
 
-linkButton :: MonadWidget t m => Dynamic t Int -> Dynamic t (Port t) -> m (Event t (Int, (Port t)))
-linkButton to newPort = do
-  (bel, _) <- el' "button" $ text "Link"
-  let clicks = domEvent Click bel
-  return $ attachDyn to $ tagDyn newPort clicks
-
-unlinkButton :: MonadWidget t m => Dynamic t Int -> Dynamic t Int -> m (Event t (Int, Int))
-unlinkButton to from = do
-  (bel, _) <- el' "button" $ text "Unlink"
+pairButton :: MonadWidget t m => String -> Dynamic t Int -> Dynamic t Int -> m (Event t (Int, Int))
+pairButton label to from = do
+  (bel, _) <- el' "button" $ text label
   let clicks = domEvent Click bel
   return $ attachDyn to $ tagDyn from clicks
 
-controlPanel :: (MonadWidget t m) => Dynamic t (Map Int (Dynamic t Node))-> m (Event t Int, Event t Int, Event t (Int, (Port t)), Event t (Int, Int))
-controlPanel state = do
-  keyNames <- forDyn state $ (fmap show) . (mapWithKey const)
+controlPanel :: (MonadWidget t m) => Dynamic t (Map Int String)-> m (Event t Int, Event t Int, Event t (Int, Int), Event t (Int, Int))
+controlPanel keyNames = do
   divClass "control" $ do
     (c, d) <- divClass "node-control" $ do
       c <- counterButton
-      d <- delButton which
       (Dropdown which _) <- dropdown 0 keyNames def
+      d <- delButton which
       return (c, d)
     (l, u) <- divClass "link-control" $ do
       text "From:"
       (Dropdown from _) <- dropdown 0 keyNames def
       text "To:"
       (Dropdown to _)  <- dropdown 0 keyNames def
-      newPort <- combineDyn getPort from state
-      l <- linkButton to newPort
-      u <- unlinkButton to from
+      l <- pairButton "Link" to from
+      u <- pairButton "Unlink" to from
       return (l, u)
     return (c, d, l, u)
-  where
-    getPort from m = maybe Map.empty (from =:) (Map.lookup from m)
 
 mkNode :: (MonadWidget t m) => Int -> Dynamic t (Port t) -> m (Dynamic t Node)
 mkNode k s = do
@@ -93,23 +83,28 @@ mkNode k s = do
 
 network :: MonadWidget t m => m ()
 network = do
-  rec (newClicked, delClicked, linkClicked, unlinkClicked) <- controlPanel stateOut
+  rec (newClicked, delClicked, linkClicked, unlinkClicked) <- controlPanel keyNames
       -- leftmost should be fine since user can't click simultaneous buttons, right?
       let stateManip = leftmost [ fmap newNode newClicked,
                                   fmap delNode delClicked,
-                                  fmap addToPort linkClicked,
+                                  fmap (addToPort stateOut) linkClicked,
                                   fmap delFromPort unlinkClicked ]
       -- stateIn describes the network of links.  It is a Map from destination node IDs to Ports.
-      (stateIn :: Dynamic t (Map Int (Port t))) <- foldDyn ($) Map.empty stateManip
+      (stateIn :: Dynamic t (Map Int (Port t))) <- foldDynM ($) Map.empty stateManip
       -- stateOut is the current state of each node.  It Maps from node ID to Node.
       (stateOut :: Dynamic t (Map Int (Dynamic t Node))) <- listWithKey stateIn mkNode
+      keyNames <- forDyn stateOut $ (fmap show) . (mapWithKey const)
   return ()
   where
-    newNode k = Map.insert k Map.empty
+    newNode k = return . (Map.insert k Map.empty)
     -- delNode is a bit ugly b/c we need to manually clean up all outgoing edges from the deleted node
-    delNode k = Map.mapMaybeWithKey (\k' p -> if (k == k') then Nothing else Just (Map.delete k p))
-    addToPort (to, p) = Map.adjust (p <>) to
-    delFromPort (to, from) = Map.adjust (Map.delete from) to
+    delNode k = return . (Map.mapMaybeWithKey (\k' p -> if (k == k') then Nothing else Just (Map.delete k p)))
+    addToPort s (to, from) m = do
+      cs <- sample $ current s
+      let newPort = getPort from cs
+      return $ (Map.adjust (newPort <>) to) m
+    delFromPort (to, from) = return . (Map.adjust (Map.delete from) to)
+    getPort from m = maybe Map.empty (from =:) (Map.lookup from m)
 
 body :: MonadWidget t m => m ()
 body = do
